30d76a05a4b22b1d1bfb2973f8272c96229af249
Dan Mushkevych <mushkevych@users.noreply.github.com>
2014-03-28 10:36:55 -0700

    Added UT for timeperiod shift by delta

--- tests/test_time_helper.py
+++ tests/test_time_helper.py
@@ -2,6 +2,7 @@ __author__ = 'Bohdan Mushkevych'
 
 import unittest
 from datetime import datetime
+
 from system import time_helper
 from system.process_context import ProcessContext
 
@@ -38,9 +39,9 @@ class TestTimeHelper(unittest.TestCase):
 
     def test_cast_to_time_qualifier(self):
         qualifiers = [ProcessContext.QUALIFIER_HOURLY, ProcessContext.QUALIFIER_HOURLY,
-                     ProcessContext.QUALIFIER_DAILY, ProcessContext.QUALIFIER_DAILY,
-                     ProcessContext.QUALIFIER_MONTHLY, ProcessContext.QUALIFIER_MONTHLY,
-                     ProcessContext.QUALIFIER_YEARLY, ProcessContext.QUALIFIER_YEARLY]
+                      ProcessContext.QUALIFIER_DAILY, ProcessContext.QUALIFIER_DAILY,
+                      ProcessContext.QUALIFIER_MONTHLY, ProcessContext.QUALIFIER_MONTHLY,
+                      ProcessContext.QUALIFIER_YEARLY, ProcessContext.QUALIFIER_YEARLY]
         params = ['2010123123', '20101231231232',
                   '2010123123', '2010123100',
                   '2010123100', '2010120000',
@@ -112,6 +113,55 @@ class TestTimeHelper(unittest.TestCase):
         expected = ['2012000000', '2013000000', '2100000000']
         for i in range(3):
             assert time_helper.increment_timeperiod(ProcessContext.QUALIFIER_YEARLY, stamps[i]) == expected[i]
+
+    def test_shift_time_by_delta(self):
+        stamps = ['2011010100', '2011010112', '2011010123']
+        expected = ['2011010103', '2011010115', '2011010202']
+        for i in range(3):
+            assert time_helper.increment_timeperiod(ProcessContext.QUALIFIER_HOURLY, stamps[i], delta=3) == expected[i]
+
+        stamps = ['2011010100', '2011010112', '2011010123']
+        expected = ['2010123121', '2011010109', '2011010120']
+        for i in range(3):
+            assert time_helper.increment_timeperiod(ProcessContext.QUALIFIER_HOURLY, stamps[i], delta=-3) == expected[i]
+
+        stamps = ['2011010100', '2011013100', '2010123100']
+        expected = ['2011010400', '2011020300', '2011010300']
+        for i in range(3):
+            assert time_helper.increment_timeperiod(ProcessContext.QUALIFIER_DAILY, stamps[i], delta=3) == expected[i]
+
+        stamps = ['2011010100', '2011013100', '2010123100']
+        expected = ['2010122900', '2011012800', '2010122800']
+        for i in range(3):
+            assert time_helper.increment_timeperiod(ProcessContext.QUALIFIER_DAILY, stamps[i], delta=-3) == expected[i]
+
+        try:
+            time_helper.increment_timeperiod(ProcessContext.QUALIFIER_MONTHLY, '2011100000', delta=2)
+            self.assertTrue(False, 'Monthly timeperiod increment for more than 1 should not be allowed')
+        except ValueError:
+            self.assertTrue(True)
+
+        try:
+            time_helper.increment_timeperiod(ProcessContext.QUALIFIER_MONTHLY, '2011100000', delta=-2)
+            self.assertTrue(False, 'Monthly timeperiod decrement for more than 1 should not be allowed')
+        except ValueError:
+            self.assertTrue(True)
+
+        stamps = ['2011010000', '2011120000', '2011100000']
+        expected = ['2010120000', '2011110000', '2011090000']
+        for i in range(3):
+            self.assertEqual(time_helper.increment_timeperiod(ProcessContext.QUALIFIER_MONTHLY, stamps[i], delta=-1),
+                             expected[i])
+
+        stamps = ['2011000000', '2012000000', '2099000000']
+        expected = ['2016000000', '2017000000', '2104000000']
+        for i in range(3):
+            assert time_helper.increment_timeperiod(ProcessContext.QUALIFIER_YEARLY, stamps[i], delta=5) == expected[i]
+
+        stamps = ['2011000000', '2012000000', '2099000000']
+        expected = ['2006000000', '2007000000', '2094000000']
+        for i in range(3):
+            assert time_helper.increment_timeperiod(ProcessContext.QUALIFIER_YEARLY, stamps[i], delta=-5) == expected[i]
 
     def test_duration_calculation(self):
         timestamp_1 = 1305934600.0
