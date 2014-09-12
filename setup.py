5bde8dbb0222c4b31fef200a47c488cf5824b5d5
Dan Muskevych <dan.mushkevych@mobidia.com>
2014-09-12 16:40:45 -0700

    Adding first version of the setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(name = 'synergy_scheduler',
+      version = '1.1',
+      description = 'Synergy Scheduler',
+      author = 'Bohdan Mushkevych',
+      author_email = 'mushkevych@gmail.com',
+      url = 'https://github.com/mushkevych/scheduler',
+      packages = ['db', 'mq', 'mx', 'scheduler', 'supervisor', 'system', 'workers'],
+      long_description = '''Really long text here.''',
+      license = 'Modified BSD License',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: JavaScript',
+          'Topic :: Communications :: Email',
+          'Topic :: Office/Business :: Scheduling',
+          'Topic :: Utilities',
+          ],
+)
