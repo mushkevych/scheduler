3cf5ecd3932cebccbcb0a32b3e731affc6d91316
Dan Muskevych <dan.mushkevych@mobidia.com>
2014-09-24 13:59:42 -0700

    Removing environment-awareness for the context.py files

--- context.py
+++ context.py
@@ -56,7 +56,102 @@ process_context = {
         token=TOKEN_LAUNCH_PY,
         time_qualifier=QUALIFIER_REAL_TIME,
         routing=ROUTING_IRRELEVANT,
-        exchange=EXCHANGE_UTILS)
+        exchange=EXCHANGE_UTILS),
+
+    PROCESS_SITE_DAILY: _process_context_entry(
+        process_name=PROCESS_SITE_DAILY,
+        classname='workers.hadoop_aggregator_driver.HadoopAggregatorDriver.start',
+        token=TOKEN_SITE,
+        time_qualifier=QUALIFIER_DAILY,
+        exchange=EXCHANGE_MANAGED_WORKER,
+        process_type=TYPE_MANAGED),
+
+    PROCESS_SITE_HOURLY: _process_context_entry(
+        process_name=PROCESS_SITE_HOURLY,
+        classname='workers.site_hourly_aggregator.SiteHourlyAggregator.start',
+        token=TOKEN_SITE,
+        time_qualifier=QUALIFIER_HOURLY,
+        exchange=EXCHANGE_MANAGED_WORKER,
+        process_type=TYPE_MANAGED,
+        source='single_session'),
+
+    PROCESS_SITE_MONTHLY: _process_context_entry(
+        process_name=PROCESS_SITE_MONTHLY,
+        classname='workers.hadoop_aggregator_driver.HadoopAggregatorDriver.start',
+        token=TOKEN_SITE,
+        time_qualifier=QUALIFIER_MONTHLY,
+        exchange=EXCHANGE_MANAGED_WORKER,
+        process_type=TYPE_MANAGED),
+
+    PROCESS_SITE_YEARLY: _process_context_entry(
+        process_name=PROCESS_SITE_YEARLY,
+        classname='workers.hadoop_aggregator_driver.HadoopAggregatorDriver.start',
+        token=TOKEN_SITE,
+        time_qualifier=QUALIFIER_YEARLY,
+        exchange=EXCHANGE_MANAGED_WORKER,
+        process_type=TYPE_MANAGED),
+
+    PROCESS_SESSION_WORKER_00: _process_context_entry(
+        process_name=PROCESS_SESSION_WORKER_00,
+        classname='workers.single_session_worker.SingleSessionWorker.start',
+        token=TOKEN_SESSION,
+        time_qualifier=QUALIFIER_REAL_TIME,
+        queue=QUEUE_RAW_DATA,
+        routing=ROUTING_IRRELEVANT,
+        exchange=EXCHANGE_RAW_DATA,
+        source='single_session',
+        sink='single_session',
+        pid_file='session_worker_00.pid',
+        log_file='session_worker_00.log'),
+
+    PROCESS_STREAM_GEN: _process_context_entry(
+        process_name=PROCESS_STREAM_GEN,
+        classname='workers.event_stream_generator.EventStreamGenerator.start',
+        token=TOKEN_STREAM,
+        time_qualifier=QUALIFIER_REAL_TIME,
+        queue=QUEUE_RAW_DATA,
+        routing=ROUTING_IRRELEVANT,
+        exchange=EXCHANGE_RAW_DATA),
+
+    PROCESS_CLIENT_DAILY: _process_context_entry(
+        process_name=PROCESS_CLIENT_DAILY,
+        classname='workers.hadoop_aggregator_driver.HadoopAggregatorDriver.start',
+        token=TOKEN_CLIENT,
+        time_qualifier=QUALIFIER_DAILY,
+        exchange=EXCHANGE_FREERUN_WORKER,
+        process_type=TYPE_MANAGED),
+
+    PROCESS_CLIENT_MONTHLY: _process_context_entry(
+        process_name=PROCESS_CLIENT_MONTHLY,
+        classname='workers.hadoop_aggregator_driver.HadoopAggregatorDriver.start',
+        token=TOKEN_CLIENT,
+        time_qualifier=QUALIFIER_MONTHLY,
+        exchange=EXCHANGE_FREERUN_WORKER,
+        process_type=TYPE_MANAGED),
+
+    PROCESS_CLIENT_YEARLY: _process_context_entry(
+        process_name=PROCESS_CLIENT_YEARLY,
+        classname='workers.hadoop_aggregator_driver.HadoopAggregatorDriver.start',
+        token=TOKEN_CLIENT,
+        time_qualifier=QUALIFIER_YEARLY,
+        exchange=EXCHANGE_FREERUN_WORKER,
+        process_type=TYPE_MANAGED),
+
+    PROCESS_ALERT_DAILY: _process_context_entry(
+        process_name=PROCESS_ALERT_DAILY,
+        classname='workers.hadoop_aggregator_driver.HadoopAggregatorDriver.start',
+        token=TOKEN_ALERT,
+        time_qualifier=QUALIFIER_DAILY,
+        exchange=EXCHANGE_MANAGED_WORKER,
+        process_type=TYPE_BLOCKING_DEPENDENCIES),
+
+    PROCESS_BASH_DRIVER: _process_context_entry(
+        process_name=PROCESS_BASH_DRIVER,
+        classname='workers.bash_driver.BashDriver.start',
+        token=TOKEN_BASH_DRIVER,
+        time_qualifier=QUALIFIER_REAL_TIME,
+        exchange=EXCHANGE_FREERUN_WORKER,
+        process_type=TYPE_FREERUN),
 }
 
 
@@ -85,14 +180,3 @@ timetable_context = {
         mx_name=TOKEN_ALERT,
         mx_page=MX_PAGE_TRAFFIC)
 }
-
-
-try:
-    from settings import ENVIRONMENT
-    overrides = __import__('context_' + ENVIRONMENT)
-except:
-    overrides = __import__('context_dev')
-
-process_context.update(overrides.process_context)
-mq_queue_context.update(overrides.mq_queue_context)
-timetable_context.update(overrides.timetable_context)
