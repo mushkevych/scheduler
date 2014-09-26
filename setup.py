e57bc166743a81fef3892e9d8f092e76f9a15ee5
Bohdan Mushkevych <mushkevych@gmail.com>
2014-09-25 22:46:02 -0700

    ongoing #16

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
 
 setup(name = 'synergy_scheduler',
-      version = '1.1',
+      version = '1.2',
       description = 'Synergy Scheduler',
       author = 'Bohdan Mushkevych',
       author_email = 'mushkevych@gmail.com',
       url = 'https://github.com/mushkevych/scheduler',
-      packages = ['db', 'db.dao', 'db.manager', 'db.model', 'mq', 'mx', 'scheduler', 'supervisor', 'system', 'workers'],
-      package_data = {'mx': ['static/*', 'templates/*'],
-                      'mq': ['AUTHORS', 'LICENSE']},
+      packages = ['synergy.db', 'synergy.db.dao', 'synergy.db.manager', 'synergy.db.model', 'synergy.mq',
+                  'synergy.mx', 'synergy.scheduler', 'synergy.supervisor', 'synergy.system', 'synergy.workers'],
+      package_data = {'synergy.mx': ['static/*', 'templates/*'],
+                      'synergy.mq': ['AUTHORS', 'LICENSE']},
       long_description = '''Really long text here.''',
       license = 'Modified BSD License',
       classifiers=[
