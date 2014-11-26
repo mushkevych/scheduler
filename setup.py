5895798d4711011af89f8c3e933f3d6d616aff91
Dan Muskevych <dan.mushkevych@mobidia.com>
2014-11-25 17:18:11 -0800

    - incrementing version to 1.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name = 'synergy_scheduler',
-      version = '1.5',
+      version = '1.6',
       description = 'Synergy Scheduler',
       author = 'Bohdan Mushkevych',
       author_email = 'mushkevych@gmail.com',
