9175791ae38f0e1f5904cc5b608e6ee370e9d11a
Bohdan Mushkevych <mushkevych@gmail.com>
2015-04-14 23:18:26 -0700

    - adding onhover event for the On/Off buttons

--- synergy/mx/static/js/mx_page.js
+++ synergy/mx/static/js/mx_page.js
@@ -57,10 +57,10 @@ function header_process_tile(process_entry, tile) {
     var is_on;
     if (process_entry.is_on) {
         is_on = '<a onclick="process_trigger(\'action/deactivate_trigger\', \'' + process_entry.process_name + '\', \'NA\', null, false, true, false)">' +
-        '<i class="fa fa-toggle-on" title="is ON"></i></a>';
+        '<i class="fa fa-toggle-on action_toogle" title="is ON"></i></a>';
     } else {
         is_on = '<a onclick="process_trigger(\'action/activate_trigger\', \'' + process_entry.process_name + '\', \'NA\', null, false, true, false)">' +
-        '<i class="fa fa-toggle-off" title="is OFF"></i></a>';
+        '<i class="fa fa-toggle-off action_toogle" title="is OFF"></i></a>';
     }
 
     var is_alive;
