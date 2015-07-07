93974f3bcf80ac58faaf47807dc5c13ca1d1a0c3
Bohdan Mushkevych <mushkevych@gmail.com>
2015-07-07 15:45:30 -0700

    - incrementing release version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='synergy_scheduler',
-      version='1.9',
+      version='1.10',
       description='Synergy Scheduler',
       author='Bohdan Mushkevych',
       author_email='mushkevych@gmail.com',
