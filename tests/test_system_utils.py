984b94fb892a9b0754190e30336c68ccba949766
Bohdan Mushkevych <mushkevych@gmail.com>
2015-09-22 15:59:32 -0700

    - addressing format() of unicoded strings

--- tests/test_system_utils.py
+++ tests/test_system_utils.py
@@ -9,14 +9,14 @@ from synergy.system.utils import unicode_truncate
 class TestSystemUtils(unittest.TestCase):
     def test_unicode_truncation(self):
         fixture = dict()
-        fixture[u"абвгдеєжзиіїйклмнопрстуфхцчшщюяь"] = u"абвгдеєжзи"
-        fixture[u"ウィキペディアにようこそ"] = u"ウィキペディ"
-        fixture["abcdefghijklmnopqrstuvwxyz"] = "abcdefghijklmnopqrst"
-        fixture["Jan 06, 2010"] = "Jan 06, 2010"
+        fixture[u'абвгдеєжзиіїйклмнопрстуфхцчшщюяь'] = u'абвгдеєжзи'
+        fixture[u'ウィキペディアにようこそ'] = u'ウィキペディ'
+        fixture['abcdefghijklmnopqrstuvwxyz'] = 'abcdefghijklmnopqrst'
+        fixture['Jan 06, 2010'] = 'Jan 06, 2010'
 
         for k, v in fixture.items():
             actual = unicode_truncate(k, 20)
-            self.assertTrue(actual == v, 'actual vs expected: {0} vs {1}'.format(actual, v))
+            self.assertTrue(actual == v, u'actual vs expected: {0} vs {1}'.format(actual, v))
 
 
 if __name__ == '__main__':
