9dd04e99aa81d97f994ca71be3201db9796992a9
Bohdan Mushkevych <mushkevych@gmail.com>
2015-09-22 16:01:00 -0700

    updating version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='synergy_scheduler',
-      version='1.13',
+      version='1.14',
       description='Synergy Scheduler',
       author='Bohdan Mushkevych',
       author_email='mushkevych@gmail.com',
