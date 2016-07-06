573b4c7fee2bad6dd86231e5498244a9ed37580b
Dan Mushkevych <mushkevych@users.noreply.github.com>
2016-07-06 09:35:51 -0700

    rephrasing the definition of the flow

--- flows.py
+++ flows.py
@@ -10,28 +10,26 @@ sa_mkdir = ShellCommandAction('mkdir -p /tmp/trash/flows')
 sa_rmdir = ShellCommandAction('rm -Rf /tmp/trash/')
 sa_identity = IdentityAction()
 
-flow_simple = FlowGraph(CLIENT_DAILY_FLOW_NAME)
-flow_simple.append(name='step_1',
-                   dependent_on_names=[],
-                   main_action=sa_rmdir, pre_actions=[sa_mkdir],
-                   post_actions=[sa_ls,  sa_mkdir])
-flow_simple.append(name='step_2',
-                   dependent_on_names=['step_1'],
-                   main_action=sa_rmdir, pre_actions=[sa_mkdir],
-                   post_actions=[sa_ls,  sa_mkdir])
-flow_simple.append(name='step_3',
-                   dependent_on_names=['step_2'],
-                   main_action=sa_rmdir, pre_actions=[sa_mkdir],
-                   post_actions=[sa_ls,  sa_mkdir])
-flow_simple.append(name='step_4',
-                   dependent_on_names=['step_2'],
-                   main_action=sa_rmdir, pre_actions=[sa_mkdir],
-                   post_actions=[sa_ls,  sa_mkdir])
-flow_simple.append(name='step_5',
-                   dependent_on_names=['step_3', 'step_4'],
-                   main_action=sa_rmdir, pre_actions=[sa_mkdir],
-                   post_actions=[sa_ls,  sa_mkdir])
-
 flows = {
-    CLIENT_DAILY_FLOW_NAME: flow_simple,
+    CLIENT_DAILY_FLOW_NAME: FlowGraph(CLIENT_DAILY_FLOW_NAME) \
+        .append(name='step_1',
+                dependent_on_names=[],
+                main_action=sa_rmdir, pre_actions=[sa_mkdir],
+                post_actions=[sa_ls, sa_mkdir]) \
+        .append(name='step_2',
+                dependent_on_names=['step_1'],
+                main_action=sa_rmdir, pre_actions=[sa_mkdir],
+                post_actions=[sa_ls, sa_mkdir]) \
+        .append(name='step_3',
+                dependent_on_names=['step_2'],
+                main_action=sa_rmdir, pre_actions=[sa_mkdir],
+                post_actions=[sa_ls, sa_mkdir]) \
+        .append(name='step_4',
+                dependent_on_names=['step_2'],
+                main_action=sa_rmdir, pre_actions=[sa_mkdir],
+                post_actions=[sa_ls, sa_mkdir]) \
+        .append(name='step_5',
+                dependent_on_names=['step_3', 'step_4'],
+                main_action=sa_rmdir, pre_actions=[sa_mkdir],
+                post_actions=[sa_ls, sa_mkdir]),
 }
