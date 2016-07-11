f15b188bca0459c71ac575edb9f4bb3b8e9cb49c
Bohdan Mushkevych <mushkevych@gmail.com>
2016-07-10 22:31:54 -0700

    ongoing integration with Synergy Flow

--- synergy/mx/static/js/mx_page.js
+++ synergy/mx/static/js/mx_page.js
@@ -161,7 +161,7 @@ function info_job_tile(job_entry, tile, is_next_timeperiod, is_selected_timeperi
         window.open(viewer_url, 'Object Viewer', 'width=800,height=480,screenX=400,screenY=200,scrollbars=1');
     });
     var flow_button = $('<button class="action_button"><i class="fa fa-random"></i>Workflow</button>').click(function (e) {
-        var params = { action: 'action/get_flow', timeperiod: job_entry.timeperiod, process_name: job_entry.process_name };
+        var params = { action: 'flow/details/flow', timeperiod: job_entry.timeperiod, process_name: job_entry.process_name };
         var viewer_url = '/viewer/flow/?' + $.param(params);
         window.open(viewer_url, 'Flow Viewer', 'width=800,height=480,screenX=400,screenY=200,scrollbars=1');
     });
